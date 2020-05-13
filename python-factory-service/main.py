7d6cfdde6e45af995e64b6b754312bc27c029cc6
jmeisele <jmeisele@yahoo.com>
2020-05-13 08:22:48 -0500

    :construction:

--- python-factory-service/main.py
+++ python-factory-service/main.py
@@ -1,20 +1,15 @@
 import pika
+from time import sleep
+sleep(15)
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq-server'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq-server'))
 channel = connection.channel()
-
 channel.queue_declare(queue='orders')
 
 def callback(ch, method, properties, body):
     print(" [x] Received order %r" % body)
 
-
-channel.basic_consume(
-    queue='orders', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='orders', on_message_callback=callback, auto_ack=True)
 
 print(' [*] Waiting for orders. To exit press CTRL+C')
 channel.start_consuming()
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', debug=True, port=3001)
