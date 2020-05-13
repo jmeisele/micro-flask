1c3cd2a593592a0d978f0fb893999fe82c7e56db
jmeisele <jmeisele@yahoo.com>
2020-05-13 10:32:34 -0500

    :rocket: doneski!

--- python-factory-service/main.py
+++ python-factory-service/main.py
@@ -6,10 +6,11 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq-se
 channel = connection.channel()
 channel.queue_declare(queue='orders')
 
-# def callback(ch, method, properties, body):
-#     print(f"[x] Received order {body}")
+exchange_name = 'product_order'
+routing_key = 'product.order.build'
+channel.queue_bind(exchange=exchange_name, queue='orders', routing_key=routing_key)
 
-def callback(body):
+def callback(ch, method, properties, body):
     print(f"[x] Received order {body}")
 
 channel.basic_consume(queue='orders', on_message_callback=callback, auto_ack=True)
